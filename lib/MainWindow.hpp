8c8ef5d0505fba626b83ee8ae11b5de389c78a69
JateX <grisaia@inbox.ru>
2023-09-26 13:54:23 +0300

    For check

--- lib/MainWindow.hpp
+++ lib/MainWindow.hpp
@@ -1,5 +1,6 @@
-#pragma once
+﻿#pragma once
 #include <QMainWindow>
+#include "stopwatch.hpp"
 #include "ui_MainWindow.h"
 
 namespace Ui { class MainWindow; }
@@ -7,8 +8,51 @@ namespace Ui { class MainWindow; }
 class MainWindow : public QMainWindow {
     Q_OBJECT
 public:
-    MainWindow(QWidget* parent = nullptr) : QMainWindow(parent), ui(new Ui::MainWindow) { ui->setupUi(this); }
+    MainWindow(QWidget* parent = nullptr) : QMainWindow(parent), ui(new Ui::MainWindow), timer(new Stopwatch(this)) { 
+        ui->setupUi(this);
+
+        // Buttons
+        ui->btn_start_stop->setText("Старт");
+        ui->btn_start_stop->setCheckable(true);
+        ui->btn_circle->setText("Круг");
+        ui->btn_circle->setEnabled(false);
+        ui->btn_clear->setText("Очистить");
+
+        // Text timer
+        ui->time->setText("00:00:00");
+
+        // Timer
+        timer->setObjectName("timer");
+    }
     ~MainWindow() { delete ui;}
+
+private slots:
+    void on_btn_clear_clicked() {
+        ui->time->setText("00:00:00");
+        ui->tb_show_ct->clear();
+    }
+
+    void timer_start() {
+    }
+
+    void on_btn_start_stop_toggled(bool) {
+        if (ui->btn_start_stop->isChecked()) {
+            ui->btn_start_stop->setText("Стоп");
+            ui->btn_circle->setEnabled(true);
+            timer->start_timer();
+
+        } else {
+            ui->btn_start_stop->setText("Старт");
+            ui->btn_circle->setEnabled(false);
+            timer->stop_timer();
+        }
+    }
+
+    void on_btn_circle_clicked() {
+        ui->tb_show_ct->append("Круг: " + QString("N") + ", Время: " + "M");
+    }
+
 private:
     Ui::MainWindow* ui;
+    Stopwatch* timer;
 };
--- lib/stopwatch.hpp
+++ lib/stopwatch.hpp
@@ -1,8 +1,16 @@
 #pragma once
 #include <QTimer>
 
-class Stopwatch : public QTimer {
+class Stopwatch : public QObject {
+	Q_OBJECT
 public:
-	Stopwatch() {}
-	~Stopwatch() = default;
+	Stopwatch(QObject* parent = nullptr) : QObject(parent), timer(new QTimer(this)) {}
+	~Stopwatch() { delete timer; }
+
+	void start_timer(void) { timer->start(1); }
+	void stop_timer(void) { timer->stop(); }
+
+private:
+	QTimer* timer;
+	int time;
 };
