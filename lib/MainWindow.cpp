d29ac12cfbcbd76dcfdcf258003ab8d64e4ea6e1
Grisaia <grisaia@inbox.ru>
2023-09-29 12:10:43 +0900

    fix warnings

--- lib/MainWindow.cpp
+++ lib/MainWindow.cpp
@@ -20,18 +20,13 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWi
 
 // slots
 void MainWindow::on_btn_clear_clicked() {
-    timer->stop_timer();
     timer->clear();
-    ui->btn_start_stop->setText("Старт");
-    ui->btn_start_stop->setChecked(false);
-    ui->l_time->setText("0:0:0");
-    //ui->tb_show_ct->clear();
+     ui->l_time->setText("0:0:0");
 }
 
 void MainWindow::on_btn_start_stop_toggled(bool) {
     if (ui->btn_start_stop->isChecked()) {
         timer->start_timer();
-        timeSendSignal();
         ui->btn_start_stop->setText("Стоп");
         ui->btn_circle->setEnabled(true);
     } else {
